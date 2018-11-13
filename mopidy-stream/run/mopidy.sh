20b75820022c17dd0c3e3a45b2cb27c32f92035a
Thomas Beyer <beyer.thomas@gmail.com>
2018-11-13 18:36:21 +0100

    changes to work with newer versions

--- mopidy-stream/run/mopidy.sh
+++ mopidy-stream/run/mopidy.sh
@@ -8,4 +8,12 @@ if [ -n "$SPOTIFY_USERNAME" ]; then
     sed -i "s/username = demo/username = $SPOTIFY_USERNAME/g" /etc/mopidy/mopidy.conf
 fi
 
+if [ -n "$SPOTIFY_ID" ]; then
+    sed -i "s/client_id = demo/client_id = $SPOTIFY_ID/g" /etc/mopidy/mopidy.conf
+fi
+
+if [ -n "$SPOTIFY_SECRET" ]; then
+    sed -i "s/client_secret = demo/client_secret = $SPOTIFY_SECRET/g" /etc/mopidy/mopidy.conf
+fi
+
 exec sudo -u mopidy mopidy --config /etc/mopidy/mopidy.conf
